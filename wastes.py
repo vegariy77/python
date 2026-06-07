1cf0179c8ae3b5db93972919c50d5db74c559a00
Igor <vegariy@mail.ru>
2026-06-07 21:51:45 +0300

    Домашнее задание (списки и кортежи)

---
+++ wastes.py
@@ -0,0 +1,7 @@
+wastes = [19, 54, 32, 70 ,32, 90 ,54]
+summ_wastes = sum(wastes)
+average_wastes = summ_wastes // len(wastes)
+max_wastes = max(wastes)
+min_wastes = min(wastes)
+wastes_tuple =(min_wastes, max_wastes, summ_wastes)
+print(wastes_tuple)
