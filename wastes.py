69448e49395bd41ea85d20b5874f78e597970185
Igor <vegariy@mail.ru>
2026-06-10 19:13:50 +0300

    Домашнее задание Строки

--- wastes.py
+++ wastes.py
@@ -1,7 +1,9 @@
 summa = input("Введите сумму: ")
 parts = summa.lower().split()
+if len(parts) != 4: 
+    print("Некорректный формат суммы")
 if parts[1] != "руб" or parts[3] != "коп":
     print("Некорректный формат")
 else:
     result = int(parts[0]) + int(parts[2]) / 100
-    print(f"{result:.2f} руб")
+    print(f"{result:.2f} ₽")
