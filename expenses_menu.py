22d5ed0c034db3b03724608f80d5995a74959763
Igor <vegariy@mail.ru>
2026-06-19 18:07:39 +0300

    Меню ф добавлением функций

--- expenses_menu.py
+++ expenses_menu.py
@@ -1,10 +1,35 @@
 expenses = []
+value = 0
+index = 0
+def add_expenses(expenses, value):
+        value = int(input("Введите расход: "))
+        return expenses.append(value)            
+def delete_expenses(expenses, index):
+        index = int(input("Введите индекс расхода который нужно удалить: "))
+        return expenses.pop(index)
+def get_total(expenses):
+        return sum(expenses)          
+def get_average(expenses):
+        return sum(expenses) // len(expenses)
+
+      
 while True:
-    print("1. Добавить расход: ")
-    print("2. Показать все расходы: " )
-    print("3. Показать сумму и средний расход: ")
-    print("4. Удалить расход по номеру: ")
-    print("5. Выход")
-    choice = int(input("Выберите действие: "))
-    if choice == 5:
-        break
+    print("1. Добавить расход")
+    print("2. Удалить раход по индексу" )
+    print("3. Сумма расходов")
+    print("4. Средний расход")
+    print("5. Напечать отчет")
+    choice = input("Выберите действие: ")
+    match choice:
+          case "1":
+                add_expenses(expenses, value)
+          case "2":
+                delete_expenses(expenses, index)
+          case "3":
+                get_total(expenses)
+          case "4":
+                get_average(expenses)
+          case "5":
+                print(f"Список расходов {expenses}")
+                print(f"""---Сумма расходов: {get_total(expenses)})---
+---Средний расход: {get_average(expenses)} ---""")
